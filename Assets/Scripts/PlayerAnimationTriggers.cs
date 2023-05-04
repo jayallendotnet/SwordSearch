a255f27b3c5c582e68410831e8f61fe69c5f326a
jayallendotnet <jayallen.net@gmail.com>
2023-05-04 10:55:08 -0400

    PowerWater animation improvements

--- Assets/Scripts/PlayerAnimationTriggers.cs
+++ Assets/Scripts/PlayerAnimationTriggers.cs
@@ -55,7 +55,7 @@ public class PlayerAnimationTriggers : MonoBehaviour{
                 o = ChooseAnimationForPowerupTypeNone();
                 break;
         }
-        //o = powerHeal;
+        o = powerLightning;
         o.SetActive(true);
             
     }
